3fa6bb901f208699c4c6abf3c632eafdb7880cd8
altamash7014 <altamash7014@gmail.com>
2026-04-14 00:57:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    string removeSpaces(string s) {
+        string result;
+        result.reserve(s.size());
+
+        for(char ch : s) {
+            if(ch != ' ') {
+                result += ch;
+            }
+        }
+        return result;
+    }
+};
