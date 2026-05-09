9725031c63f9439ed921805d6f2d63d1110a0b0e
altamash7014 <altamash7014@gmail.com>
2026-05-09 21:34:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Increment a variable/increment-a-variable.cpp
@@ -0,0 +1,10 @@
+/* This function is invoked by passing the address of the variable.
+    You need to increment the value stored in the variable by 10 */
+
+void updateVar(int  *a)
+
+{
+
+*a+=10;
+
+}
