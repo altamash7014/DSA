8d535ffc0b47d53ce2b5f60a8d31d4218837ae1b
altamash7014 <altamash7014@gmail.com>
2026-08-05 20:17:15 +0530

    Time: 13 ms (36.71%) | Memory: 14 MB (49.99%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& arr, int target) {
-
-        sort(arr.begin(), arr.end());
-
-        int n = arr.size();
-        int ans = arr[0] + arr[1] + arr[2];
-
-        for (int i = 0; i < n - 2; i++) {
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-
+    int n  = arr.size();
+        // int right = n-1;
+        // int left = 0;
+        int sum=0;
+        int mini = INT_MAX;
+        int count =0;
+        int ans =0;
+        sort(arr.begin(),arr.end());
+        for(int i =0;i<n-2;i++){
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+            int left =i+1;
+            int right = n-1;
+            while(left<right){
                 int sum = arr[i] + arr[left] + arr[right];
+int diff = abs(target - sum);
 
-                if (abs(target - sum) < abs(target - ans))
-                    ans = sum;
+if(diff < mini){
+    ans = sum;
+    mini = diff;
+}
 
-                if (sum == target)
-                    return target;
-                else if (sum < target)
-                    left++;
-                else
-                    right--;
+if(sum == target)
+    return target;
+else if(sum < target)
+    left++;
+else
+    right--;
             }
-        }
-
-        return ans;
+        }return ans;        
+                // mini = min(diff,mini);
     }
 };
