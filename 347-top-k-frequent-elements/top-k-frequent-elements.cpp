cbc7520afd2998efc66c50298a740540219fa4da
altamash7014 <altamash7014@gmail.com>
2026-08-08 16:50:50 +0530

    Time: 8 ms (8.12%) | Memory: 18 MB (66.92%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& arr, int k) {
+
+        unordered_map<int, int> mp;
+
+        for(int i : arr){
+            mp[i]++;
+        }
+
+        vector<pair<int,int>> v;
+
+        for(auto it : mp){
+            v.push_back({it.second, it.first});
+        }
+
+        sort(v.begin(), v.end(), greater<pair<int,int>>());
+
+        vector<int> ans;
+
+        for(int i = 0; i < k; i++){
+            ans.push_back(v[i].second);
+        }
+
+        return ans;
+    }
+};
